29c2b71c773d6588f887aa85f919a5dae569a118
Corpse <89634669+ItsCorpse@users.noreply.github.com>
2021-09-05 14:13:44 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
-//function 
-function add(num1,num2) {
-let result num1 + num2;
-return result;
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
 }
